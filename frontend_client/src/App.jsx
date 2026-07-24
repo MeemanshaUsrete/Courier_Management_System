c2ec23a31b5697fbd7373ccb1fa1e23f81773a01
Riya Rahangdale <riyarahangdale09@gmail.com>
2026-07-25 00:27:37 +0530

    Profile Page Added

--- frontend_client/src/App.jsx
+++ frontend_client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
   );
