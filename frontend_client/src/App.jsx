d11ad0ff294eff744077dfc7ffe58bb320a7c332
Riya Rahangdale <riyarahangdale09@gmail.com>
2026-07-24 15:56:54 +0530

    Page Updated

--- frontend_client/src/App.jsx
+++ frontend_client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup"
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
       </Routes>
     </BrowserRouter>
